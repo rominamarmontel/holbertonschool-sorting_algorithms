16e159f9648b200d8a60b67c969f105d2cc803da
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-12 19:42:35 +0200

    sort.h

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void quick_sort_part(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 #endif
