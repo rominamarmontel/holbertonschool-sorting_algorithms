8e60f29666215b4251b5b31978115990427408ae
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-13 13:47:09 +0200

    3rd push

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -25,7 +25,7 @@ void quick_sort_part(int *array, int left, int right, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2 || array == NULL)
+	if (size < 2)
 		return;
 	quick_sort_part(array, 0, size - 1, size);
 }
@@ -55,9 +55,12 @@ int partition(int *array, int left, int right, size_t size)
 			}
 		}
 	}
-	swap(&array[x + 1], &array[right]);/*swap index x +1 and pivot*/
-	print_array(array, size);
-	return (x + 1);
+	if (array[right] < array[x + 1])
+	{
+		swap(&array[x + 1], &array[right]);/*swap index x +1 and pivot*/
+		print_array(array, size);
+		return (x + 1);
+	}
 }
 /**
  * swap - swap the 2 elements
