24d3d72416e6a22efbd0c5fd55f23cb8d1be76e1
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-13 18:35:22 +0200

    test

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void quick_sort(int *array, size_t size);
 void quick_sort_part(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
+void pushdown (int array[], int first, int last);
 
 #endif
