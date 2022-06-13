ce043309483e735ac3e38bc3333ff5766fb3e27c
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-13 10:01:07 +0200

    second push

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
@@ -27,4 +27,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_part(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
+
 #endif
